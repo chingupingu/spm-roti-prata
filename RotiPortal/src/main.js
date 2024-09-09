7d88f48c82cb18e5ef92e69f8ac2548bf23d2102
chingupingu <p.shuen01@gmail.com>
2024-09-09 18:01:11 +0800

    hello world

---
+++ RotiPortal/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import './style.css'
+import App from './App.vue'
+
+// const router = createRouter({
+//     // history: createWebHistory(import.meta.env.BASE_URL),
+//     routes: [
+//         {path: '/', name: 'Home, component: Home'}
+//     ]
+// })
+
+createApp(App).mount('#app')
