ea738389ba87ed479a10a99623075f5b81138d80
chingupingu <p.shuen01@gmail.com>
2024-09-10 11:30:54 +0800

    login basic function

--- RotiPortal/src/router/index.js
+++ RotiPortal/src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
       component: Login
     },
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home
     },
--- RotiPortal/src/views/Login.vue
+++ RotiPortal/src/views/Login.vue
@@ -11,8 +11,8 @@
                 <div style="width: 400px;" class="bg-light rounded-3 p-3">
                     <h1 class="text-center">Login</h1>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email:</label>
-                        <input required v-model="email" type="email" class="form-control" id="email">
+                        <label for="username" class="form-label">Username:</label>
+                        <input required v-model="username" type="text" class="form-control" id="username">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password:</label>
@@ -38,46 +38,59 @@ export default {
     name: 'login',
     data() {
         return {
-            email: '',
+            username: '',
             password: '',
             errorMsg: '',
         }
     },
     methods: {
         authenticate() {
-            this.errorMsg = ''
-            const data = {
-                email: this.email,
-                password: this.password
+            this.errorMsg = ""
+            if (this.password == "0000") {
+                if (this.username == "staff") {
+                    sessionStorage.setItem("user", "staff")
+                    this.redirect()
+                } else if (this.username == "manager") {
+                    sessionStorage.setItem("user", "manager")
+                    this.redirect()
+                } else if(this.username == "hr") {
+                    sessionStorage.setItem("user", "hr")
+                    this.redirect()
+                } else {
+                    this.errorMsg = "This user does not exist."
+                }
+            } else {
+                this.errorMsg = "Your password is incorrect."
             }
-            axios.post("http://localhost:8000/user/auth?apikey=admin", data)
-                // axios.post("http://localhost:5010/user/auth", data)
-                .then(response => {
-                    if (response.data.code == 401) {
-                        this.errorMsg = response.data.message
-                    }
-                    else if (response.data.code == 201) {
-                        this.errorMsg = ''
-                        sessionStorage.setItem('isLoggedIn', 'true')
-                        sessionStorage.setItem('user', JSON.stringify(response.data.data))
-                        this.redirect(JSON.parse(sessionStorage.getItem('user')).is_creator)
-                    }
-                })
-                .catch(error => {
-                    if (error.code == "ERR_BAD_REQUEST") {
-                        this.errorMsg = 'User not found in database'
-                    }
-                })
+                
+            // this.errorMsg = ''
+            // const data = {
+            //     email: this.email,
+            //     password: this.password
+            // }
+            // axios.post("http://localhost:8000/user/auth?apikey=admin", data)
+            //     // axios.post("http://localhost:5010/user/auth", data)
+            //     .then(response => {
+            //         if (response.data.code == 401) {
+            //             this.errorMsg = response.data.message
+            //         }
+            //         else if (response.data.code == 201) {
+            //             this.errorMsg = ''
+            //             sessionStorage.setItem('isLoggedIn', 'true')
+            //             sessionStorage.setItem('user', JSON.stringify(response.data.data))
+            //             this.redirect(JSON.parse(sessionStorage.getItem('user')).is_creator)
+            //         }
+            //     })
+            //     .catch(error => {
+            //         if (error.code == "ERR_BAD_REQUEST") {
+            //             this.errorMsg = 'User not found in database'
+            //         }
+            //     })
         },
 
 
-        redirect(is_creator) {
-            if (is_creator) {
-                this.$router.push({ path: `/creator`, replace: true })
-            }
-            else {
-                this.$router.push({ path: `/backer`, replace: true })
-            }
+        redirect() {
+            this.$router.push({ path: `/home`, replace: true })
         }
 
     },
